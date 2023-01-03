fd15d945d2425e4300daf2ce31a0864132422689
LesCrow <john.baruteau@gmail.com>
2023-01-03 22:01:57 +0100

    logo, search picto and profile picture added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import Navbar from "../src/components/Navbar";
+
 export default function Home() {
-  return <div>TEMPLATE</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../../public/assets/logo-enedis-share_blanc.png";
+import pictoSearch from "../../public/assets/ENEDIS_PICTO_003_Search_BLANC_EXE.png";
+import profilePicture from "../../public/assets/john-min.JPG";
+
+function Navbar() {
+  return (
+    <div className="h-[64px] bg-bleu flex justify-center">
+      <div className="w-[95%]  flex justify-between items-center">
+        <img src={logo.src} alt="enedis-share-logo" className="w-1/3 " />
+        <div className="flex  justify-between w-[30%]">
+          <div className="bg-vert w-[45px] h-[45px] rounded-full flex justify-center items-center">
+            <img
+              src={pictoSearch.src}
+              alt="search-picto"
+              className="w-[80%] h-[80%]"
+            />
+          </div>
+          <div className="bg-vert w-[45px] h-[45px] rounded-full flex justify-center items-center">
+            {" "}
+            <img
+              src={profilePicture.src}
+              alt="profile"
+              className="rounded-[50%] h-[40px] w-[40px] object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
