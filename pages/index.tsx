e647fccd37fedaa390f84ce9542411b85adfeb0b
LesCrow <john.baruteau@gmail.com>
2023-01-03 22:01:57 +0100

    logo, search picto and profile picture added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Footer from "../src/components/footer";
 
 import MyAccountMobile from "../src/components/MyAccountMobile";
 
+import Navbar from "../src/components/Navbar";
+
 export default function Home() {
   return (
     <div>
