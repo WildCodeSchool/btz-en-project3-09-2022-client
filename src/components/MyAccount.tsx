afc060d86c2d23952c2feec03206505d93a6cb18
LesCrow <john.baruteau@gmail.com>
2023-01-03 23:57:09 +0100

    design in progress

--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import profilPicture from "../../public/assets/john-min.JPG";
+import profilPicture from "../../public/assets/john-min.jpg";
+import pictoSettings from "../../public/assets/image-3.png";
 
 function MyAccount() {
   return (
-    <div className="bg-lightBlue h-full">
+    <div className="bg-bgLightBlue h-full">
       <div className="flex flex-col items-center ">
         <img
           src={profilPicture.src}
@@ -11,53 +12,54 @@ function MyAccount() {
           className="w-[165px] h-[165px] rounded-[50%] my-[5%]"
         />
         <div className="w-full flex flex-col items-center space-y-3">
-          <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+          <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
+            <img src={pictoSettings.src} alt="settings" className="w-[32px]" />
             Modifier mes infos
           </p>
-          <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+          <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
             Prénom NOM
           </p>
-          <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+          <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
             nom équipe
           </p>
-          <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+          <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
             nom site
           </p>
-          <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+          <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
             email
           </p>
-          <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+          <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
             anniversaire
           </p>
           <div className="pt-5 w-full flex flex-col items-center space-y-3">
-            <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+            <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
               Mon équipe
             </p>
-            <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+            <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
               personne 1
             </p>
             <p>voir plus...</p>
           </div>
           <div className="pt-5 w-full flex flex-col items-center space-y-3">
-            <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+            <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
               Mes groupes
             </p>
-            <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+            <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
               groupe 1
             </p>
             <p>voir plus...</p>
           </div>
           <div className="pt-5 w-full flex flex-col items-center space-y-3">
-            <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+            <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
               Mes publications
             </p>
-            <p className="w-1/2 border rounded-md  text-center h-[40px] bg-white">
+            <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
               groupe 1
             </p>
             <p>voir plus...</p>
           </div>
           <div className="pt-5 w-full flex flex-col items-center space-y-3">
-            <p className="flex justify-center items-center w-1/2 border rounded-md  h-[40px] bg-white">
+            <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
               Mon activité
             </p>
             <p>J&apos;ai commenté X truc de machin...</p>
