7f6f5845006da0de5f48381b42cb5135279f4844
LesCrow <john.baruteau@gmail.com>
2023-01-03 22:43:49 +0100

    search bar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../public/assets/logo-enedis-share_blanc.png";
 import pictoSearch from "../../public/assets/ENEDIS_PICTO_003_Search_BLANC_EXE.png";
 import profilePicture from "../../public/assets/john-min.JPG";
 
 function Navbar() {
+  // display search bar
+  const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
+
+  const displaySearchBar = () => {
+    setIsSearchBarOpen(!isSearchBarOpen);
+  };
+
   return (
     <div className="h-[64px] bg-bleu flex justify-center">
       <div className="w-[95%]  flex justify-between items-center">
@@ -13,15 +20,27 @@ function Navbar() {
           className="max-w-[30%] min-w-[100px]"
         />
         <div className="flex justify-between min-w-[100px] ">
-          <div className="bg-vert w-[45px] h-[45px] rounded-full flex justify-center items-center">
-            <img
-              src={pictoSearch.src}
-              alt="search-picto"
-              className="w-[80%] h-[80%]"
-            />
+          <div className="flex flex-row-reverse  justify-around items-center ">
+            <button
+              type="button"
+              className="bg-vert w-[45px] h-[45px] rounded-full flex justify-center items-center"
+              onClick={displaySearchBar}
+            >
+              <img
+                src={pictoSearch.src}
+                alt="search-picto"
+                className="w-[80%] h-[80%]"
+              />
+            </button>
+
+            {isSearchBarOpen && (
+              <input
+                className="w-2/3 h-1/2 rounded-full"
+                placeholder="recherche.."
+              />
+            )}
           </div>
-          <div className="bg-vert w-[45px] h-[45px] rounded-full flex justify-center items-center">
-            {" "}
+          <div className="bg-vert min-w-[45px] h-[45px] rounded-full flex justify-center items-center">
             <img
               src={profilePicture.src}
               alt="profile"
