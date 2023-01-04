397892cae9a27ba535c3af5254a5f56b563247f6
Damsbot64 <damien64durand@gmail.com>
2023-01-04 12:27:34 +0100

    adding popup to espace

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 function Footer() {
+  const [openEspace, setOpenEspace] = useState(false);
+  const handleClickEspace = () => {
+    setOpenEspace(!openEspace);
+  };
+
   return (
-    <div className="bg-bleu flex justify-around p-1 w-full">
-      <Link href="/">
+    <div>
+      <div>
+        {openEspace && (
+          <div className="h-2/3 bg-slate-400 flex justify-center items-start p-4">
+            <div className="bg-bleu w-2/6 rounded-full text-white flex justify-center py-2">
+              Mes espaces
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="bg-bleu flex justify-around p-1 w-full">
+        <Link href="/">
+          <Image
+            src="/logo_enedis/enedis-home.png"
+            width={75}
+            height={75}
+            alt="logo homepage"
+          />
+        </Link>
+
         <Image
-          src="/logo_enedis/enedis-home.png"
+          src="/logo_enedis/enedis-espace.png"
           width={75}
           height={75}
-          alt="logo homepage"
+          alt="logo carte des espaces"
+          onClick={handleClickEspace}
         />
-      </Link>
-
-      <Image
-        src="/logo_enedis/enedis-espace.png"
-        width={75}
-        height={75}
-        alt="logo carte des espaces"
-      />
-      <Image
-        src="/logo_enedis/enedis-category.png"
-        width={75}
-        height={75}
-        alt="logo choix categories"
-      />
-      <Image
-        src="/logo_enedis/enedis-notifications.png"
-        width={75}
-        height={75}
-        alt="logo des notifications"
-      />
+        <Image
+          src="/logo_enedis/enedis-category.png"
+          width={75}
+          height={75}
+          alt="logo choix categories"
+        />
+        <Image
+          src="/logo_enedis/enedis-notifications.png"
+          width={75}
+          height={75}
+          alt="logo des notifications"
+        />
+      </div>
     </div>
   );
 }
