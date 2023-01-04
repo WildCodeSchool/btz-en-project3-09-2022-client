3d8971fb23ed44e85ded12e0ceae6ff9061a853f
Damsbot64 <damien64durand@gmail.com>
2023-01-04 11:33:28 +0100

    link home

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,27 +1,36 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 function Footer() {
   return (
     <div className="bg-bleu flex justify-around p-1 w-full">
-      <img
-        src="/logo_enedis/enedis-home.png"
-        alt="logo homepage"
-        className="w-1/12 h-1/2"
-      />
-      <img
+      <Link href="/">
+        <Image
+          src="/logo_enedis/enedis-home.png"
+          width={75}
+          height={75}
+          alt="logo homepage"
+        />
+      </Link>
+
+      <Image
         src="/logo_enedis/enedis-espace.png"
+        width={75}
+        height={75}
         alt="logo carte des espaces"
-        className="w-1/12"
       />
-      <img
+      <Image
         src="/logo_enedis/enedis-category.png"
+        width={75}
+        height={75}
         alt="logo choix categories"
-        className="w-1/12"
       />
-      <img
+      <Image
         src="/logo_enedis/enedis-notifications.png"
+        width={75}
+        height={75}
         alt="logo des notifications"
-        className="w-1/12"
       />
     </div>
   );
