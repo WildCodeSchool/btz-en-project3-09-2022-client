7adfffb0e49dc9f855e0ed97ce72f6d96c865c5e
LesCrow <john.baruteau@gmail.com>
2023-01-04 12:12:30 +0100

    waiting for connection page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const reactQueryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={reactQueryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -1,19 +1,46 @@
+/* eslint-disable no-console */
+
 import React from "react";
-import profilPicture from "../../public/assets/john-min.jpg";
-import pictoSettings from "../../public/assets/image-3.png";
+import { useQuery } from "react-query";
+
+import axios from "axios";
 
 function MyAccount() {
+  // Get users
+  const getOneUser = async () => {
+    try {
+      const user = await axios.get(`http://localhost:4000/api/v1/users/`);
+      return user.data;
+    } catch (error) {
+      console.log(error);
+      return "";
+    }
+  };
+
+  // UseQuery
+  const { isLoading, data: user, error } = useQuery("user", getOneUser);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    <p>Something bad happen</p>;
+  }
+
+  console.log(user);
+
   return (
     <div className="bg-bgLightBlue h-full">
       <div className="flex flex-col items-center ">
         <img
-          src={profilPicture.src}
+          src="/assets/john-min.jpg"
           alt="profil"
-          className="w-[165px] h-[165px] rounded-[50%] my-[5%]"
+          className="w-[165px] h-[165px] rounded-[50%] my-[5%] object-cover"
         />
         <div className="w-full flex flex-col items-center space-y-3">
           <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
-            <img src={pictoSettings.src} alt="settings" className="w-[32px]" />
+            <img src="/assets/image-3.png" alt="settings" width={32} />
             Modifier mes infos
           </p>
           <p className="flex justify-center items-center w-1/2 border border-lightBlue rounded-lg  h-[40px] bg-white">
