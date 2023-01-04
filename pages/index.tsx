089141d099a099b2090bc6bcf9438844e46521af
Damsbot64 <damien64durand@gmail.com>
2023-01-04 16:13:47 +0100

    continuing the footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Footer from "../src/components/footer";
 export default function Home() {
   return (
     <div className="flex flex-col justify-end h-screen">
-      TEMPLATE
       <Footer />
     </div>
   );
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Link from "next/link";
 import Image from "next/image";
 
 function Footer() {
@@ -10,42 +9,48 @@ function Footer() {
 
   return (
     <div>
+      <hr className="bg-vert h-1" />
       <div>
         {openEspace && (
-          <div className="h-2/3 bg-slate-400 flex justify-center items-start p-4">
-            <div className="bg-bleu w-2/6 rounded-full text-white flex justify-center py-2">
+          <div className="h-2/3 bg-bleu_leger flex flex-col justify-center items-center p-2 ">
+            <div className=" w-2/6  text-black font-bold flex justify-center ">
               Mes espaces
             </div>
+            <hr className="bg-bleu h-1 w-1/3 rounded-full" />
           </div>
         )}
       </div>
-      <div className="bg-bleu flex justify-around p-1 w-full">
-        <Link href="/">
-          <Image
-            src="/logo_enedis/enedis-home.png"
-            width={75}
-            height={75}
-            alt="logo homepage"
-          />
-        </Link>
 
+      <div className="bg-bleu flex justify-around p-1 w-full">
         <Image
-          src="/logo_enedis/enedis-espace.png"
-          width={75}
-          height={75}
-          alt="logo carte des espaces"
-          onClick={handleClickEspace}
-        />
-        <Image
-          src="/logo_enedis/enedis-category.png"
-          width={75}
-          height={75}
+          src="/logo_enedis/picto_profil_vert.svg"
+          width={28}
+          height={40}
           alt="logo choix categories"
         />
+        {openEspace ? (
+          <Image
+            src="/logo_enedis/picto_espace_white.svg"
+            width={40}
+            height={40}
+            alt="logo carte des espaces"
+            onClick={handleClickEspace}
+            className="bg-white rounded-md p-1"
+          />
+        ) : (
+          <Image
+            src="/logo_enedis/picto_espace_vert.svg"
+            width={40}
+            height={40}
+            alt="logo carte des espaces"
+            onClick={handleClickEspace}
+          />
+        )}
+
         <Image
-          src="/logo_enedis/enedis-notifications.png"
-          width={75}
-          height={75}
+          src="/logo_enedis/picto_notif_vert.svg"
+          width={40}
+          height={40}
           alt="logo des notifications"
         />
       </div>
