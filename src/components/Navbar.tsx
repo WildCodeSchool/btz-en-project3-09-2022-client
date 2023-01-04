a04a11e08b777c772f735f586900ac0bc912ce14
LesCrow <john.baruteau@gmail.com>
2023-01-04 16:17:20 +0100

    search bar + modal

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import logo from "../../public/assets/logo-enedis-share_blanc.png";
 import pictoSearch from "../../public/assets/ENEDIS_PICTO_003_Search_BLANC_EXE.png";
 import profilePicture from "../../public/assets/john-min.JPG";
+import useModal from "./modal/useModal";
+import Modal from "./modal/Modal";
 
 function Navbar() {
   // display search bar
@@ -11,19 +13,28 @@ function Navbar() {
     setIsSearchBarOpen(!isSearchBarOpen);
   };
 
+  // Modal
+  const { isShowing, toggle } = useModal();
+
   return (
-    <div className="h-[64px] bg-bleu flex justify-center">
+    <div className="h-[100px] bg-bleu flex justify-center relative">
       <div className="w-[95%]  flex justify-between items-center">
         <img
           src={logo.src}
           alt="enedis-share-logo"
-          className="max-w-[30%] min-w-[100px]"
+          className="max-w-[10%] min-w-[200px]"
         />
         <div className="flex justify-between min-w-[100px] ">
-          <div className="flex flex-row-reverse  justify-around items-center ">
+          <div className=" flex flex-row-reverse  justify-around items-center ">
+            {isSearchBarOpen && (
+              <input
+                className="absolute right-[75px] md:right-[85px] lg:right-[95px] xl:right-[105px] w-1/3 h-[45px] rounded-full text-center "
+                placeholder="recherche..."
+              />
+            )}
             <button
               type="button"
-              className="bg-vert w-[45px] h-[45px] rounded-full flex justify-center items-center"
+              className="bg-vert w-[45px] h-[45px] rounded-full flex justify-center items-center z-10"
               onClick={displaySearchBar}
             >
               <img
@@ -32,22 +43,34 @@ function Navbar() {
                 className="w-[80%] h-[80%]"
               />
             </button>
-
-            {isSearchBarOpen && (
-              <input
-                className="w-2/3 h-1/2 rounded-full text-center"
-                placeholder="recherche.."
-              />
-            )}
           </div>
           <div className="bg-vert min-w-[45px] h-[45px] rounded-full flex justify-center items-center">
-            <img
-              src={profilePicture.src}
+            {/* <Image
+              src="/assets/john-min.JPG"
+              width={80}
+              height={80}
               alt="profile"
-              className="rounded-[50%] h-[40px] w-[40px] object-cover"
-            />
+              className="rounded-[50%] object-cover"
+            /> */}
+            <button type="button" onClick={toggle}>
+              <img
+                src={profilePicture.src}
+                alt="profile"
+                className="rounded-[50%] h-[40px] w-[40px] object-cover"
+              />
+            </button>
           </div>
         </div>
+      </div>
+      <div className="absolute">
+        <Modal isShowing={isShowing} hide={toggle}>
+          <div className="space-y-2 ">
+            <p className="text-white">Mon profil</p>
+            <p className="text-white">Paramètres</p>
+            <p className="text-white">Aide</p>
+            <p className="text-white">Me déconnecter</p>
+          </div>
+        </Modal>
       </div>
     </div>
   );
