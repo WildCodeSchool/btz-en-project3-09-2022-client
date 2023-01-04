79bd43888839a4d2f625f9f9c81b08331ac0fdfa
Damsbot64 <damien64durand@gmail.com>
2023-01-04 17:20:26 +0100

    working on getting data

---
+++ src/components/espace.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+const getAllSpaces = async () => {
+  const res = await axios.get("http://localhost:4000/api/v1/spaces");
+  return console.log(res.data);
+};
+
+function Espace() {
+  return <div>aa</div>;
+}
+
+export default Espace;
