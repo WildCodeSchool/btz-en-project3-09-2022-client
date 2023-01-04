080d7ce05ada578d40521045ab1387f8c0993ce5
Damsbot64 <damien64durand@gmail.com>
2023-01-04 17:21:17 +0100

    working on getting data

--- src/components/espace.tsx
+++ src/components/espace.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import axios from "axios";
 
-const getAllSpaces = async () => {
-  const res = await axios.get("http://localhost:4000/api/v1/spaces");
-  return console.log(res.data);
-};
+// const getAllSpaces = async () => {
+//   const res = await axios.get("http://localhost:4000/api/v1/spaces");
+//   return console.log(res.data);
+// };
 
 function Espace() {
   return <div>aa</div>;
