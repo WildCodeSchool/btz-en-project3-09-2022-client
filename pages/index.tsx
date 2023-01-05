3d270c1c70498902a87a08bc512b769d848c61ac
LesCrow <john.baruteau@gmail.com>
2023-01-05 22:27:22 +0100

    glabals.css, l.43, /Enedis-Black to Enedis-Regular

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,75 @@
 export default function Home() {
-  return <div>TEMPLATE</div>;
+  // return (
+  //   <div>
+  //     <h1 className="font-enedis text-desk-xxs(mention)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-desk-xs(date)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-desk-sm(textPost+multiuse)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-desk-md(titlePubli+multiuse)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-desk-lg(CTA+input)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-desk-xl(section)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-desk-2xl(headerCard)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-desk-3xl(header+name)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-desk-4xl(welcome)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-mob-xxs(mention+date)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-mob-xs(textPost)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-mob-sm(multiuse)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-mob-md(CTA+input)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-mob-lg(multiuse)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-mob-xl(headers+titles)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-mob-3xl(welcome+name)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h1 className="font-enedis text-mob-4xl(welcomeConnect)">
+  //       Description de l’espace
+  //     </h1>
+  //     <h2 className="font-publicSans font-light">
+  //       Je veux ajouter comme membre :
+  //     </h2>
+  //     <h2 className="font-publicSans font-regular">
+  //       Je veux ajouter comme membre :
+  //     </h2>
+  //     <h2 className="font-publicSans font-bold">
+  //       Je veux ajouter comme membre :
+  //     </h2>
+  //     <div className="bg-card-lightgrey-backNeumorphism w-full flex-all-center space-y-4">
+  //       <div className="w-40 h-20 bg-cta-orange rounded-none bg-background-enedis" />
+  //       <div className="w-40 h-20 bg-cta-orange rounded-full bg-blue-enedis" />
+  //       <div className="w-40 h-20 bg-cta-orange rounded-app-bloc bg-green-enedis" />
+  //       <div className="w-40 h-20 bg-cta-orange rounded-connection-bloc bg-dark-enedis" />
+  //       <div className="w-40 h-20 bg-cta-orange rounded-select-desk bg-white-enedis" />
+  //       <div className="w-40 h-20 bg-cta-orange rounded-select-mobile bg-yellow-enedis" />
+  //       <div className="w-40 h-20 bg-cta-orange rounded-upload-area" />
+  //     </div>
+  //   </div>
+  // );
 }
