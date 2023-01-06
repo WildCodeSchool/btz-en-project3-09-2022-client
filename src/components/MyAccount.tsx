93eb8524d0a5b5d068d679fb483895b0f89f42df
LesCrow <john.baruteau@gmail.com>
2023-01-06 11:58:20 +0100

    design done

--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -28,15 +28,13 @@ function MyAccount() {
     <p>Something bad happen</p>;
   }
 
-  console.log(user);
-
   return (
     <div className="flex flex-col items-center pb-8">
-      <div className="bg-bgLightBlue w-[95%] m-auto mt-2 pb-10 ">
+      <div className="bg-background-enedis w-[95%] m-auto mt-2 pb-10 ">
         <div className="flex flex-col items-center pt-6">
-          <h3>Mon profil</h3>
-          <hr className="h-[6px] w-2/3 rounded-full bg-bleu mb-4" />
-          <div>
+          <h3 className="mb-2">Mon profil</h3>
+          <hr className="h-[6px] w-2/3 rounded-full bg-blue-enedis mb-4" />
+          <div className="flex">
             <img
               src="/assets/john-min.jpg"
               alt="profil"
@@ -44,13 +42,13 @@ function MyAccount() {
             />
             <button
               type="button"
-              className="absolute top-52 text-center px-2 pt-1 w-[165px] rounded-full h-[53px] bg-vert text-white"
+              className=" absolute top-60  text-center px-2  w-[165px] rounded-full h-[53px] bg-green-enedis text-white-enedis"
             >
               Changer <br /> ma photo de profil
             </button>
           </div>
           <div className="w-full  flex flex-col items-center  mt-14 space-y-3">
-            <p className="flex items-center  w-3/4 border border-bleu rounded-full  h-[32px] cursor-not-allowed ">
+            <p className="flex items-center  w-5/6 border border-blue-enedis rounded-full  h-[32px] cursor-not-allowed ">
               <Image
                 src="/assets/ENEDIS_PICTO_020_Homme_BLEU_RVB_EXE 1.png"
                 width={25}
@@ -60,7 +58,7 @@ function MyAccount() {
               />{" "}
               Prénom
             </p>
-            <p className="flex items-center  w-3/4 border border-bleu rounded-full  h-[32px] cursor-not-allowed ">
+            <p className="flex items-center  w-5/6 border border-blue-enedis rounded-full  h-[32px] cursor-not-allowed ">
               <Image
                 src="/assets/ENEDIS_PICTO_020_Homme_BLEU_RVB_EXE 1.png"
                 width={25}
@@ -70,7 +68,7 @@ function MyAccount() {
               />{" "}
               Nom
             </p>
-            <p className="flex items-center  w-3/4 border border-bleu rounded-full  h-[32px] cursor-not-allowed ">
+            <p className="flex items-center  w-5/6 border border-blue-enedis rounded-full  h-[32px] cursor-not-allowed ">
               <Image
                 src="/assets/picto-birthday 1.png"
                 width={25}
@@ -80,15 +78,15 @@ function MyAccount() {
               />{" "}
               XX/XX/XXXX
             </p>
-            <div className="flex pt-4  w-3/4 justify-between items-center">
-              <p>Montrer ma date de naissance</p>
+            <div className="flex pt-4  w-5/6 justify-between items-center ">
+              <p className="">Montrer ma date de naissance</p>
 
               <label className="switch" htmlFor="showBirthday">
                 <input type="checkbox" id="showBirthday" />
                 <span className="slider round" />
               </label>
             </div>
-            <div className="flex  w-3/4 justify-between items-center">
+            <div className="flex  w-5/6 justify-between items-center">
               <p>Montrer mon adresse email</p>
 
               <label className="switch" htmlFor="showEmail">
@@ -97,13 +95,13 @@ function MyAccount() {
               </label>
             </div>
             <div className="w-full flex flex-col items-center pt-4 pb-4">
-              <h3>Ma connexion</h3>
-              <hr className="h-[6px] w-2/3 rounded-full bg-bleu" />
+              <h3 className="mb-2">Ma connexion</h3>
+              <hr className="h-[6px] w-2/3 rounded-full bg-blue-enedis" />
             </div>
-            <p className="text-left w-3/4 ">
+            <p className="text-left w-5/6 pt-4">
               J&apos;utilise l&apos;adresse email :
             </p>
-            <p className="flex items-center  w-3/4 border border-bleu rounded-full h-[32px] cursor-not-allowed ">
+            <p className="flex items-center  w-5/6 border border-blue-enedis rounded-full h-[32px] cursor-not-allowed ">
               <Image
                 src="/assets/ENEDIS_PICTO_018_Contact_BLEU_RVB_EXE 1.png"
                 width={25}
@@ -113,31 +111,33 @@ function MyAccount() {
               />{" "}
               john.doe@enedis.fr
             </p>
-            <p className="text-left w-3/4 ">Je change mon mot de passe :</p>
+            <p className="text-left w-5/6 pt-6 ">
+              Je change mon mot de passe :
+            </p>
             <input
               type="password"
               placeholder="Mon nouveau mot de passe"
-              className="rounded-full h-[32px] pl-6 border-2"
+              className="w-5/6 rounded-full h-[32px] pl-6 border-2"
             />
             <input
               type="password"
               placeholder="Confirmer mon mot de passe"
-              className="rounded-full h-[32px] pl-6 border-2"
+              className="w-5/6 rounded-full h-[32px] pl-6 border-2"
             />
-            <p className="text-left w-3/4">
+            <p className="text-left text-mob-xxs(mention+date) w-5/6 ">
               Le mot de passe doit contenir au moins 8 caractères, dont au
-              moins: 1chiffre, 1 lettre en majuscule, 1 lettre en minuscule et 1
-              caractère spécial.
+              moins: 1 chiffre, 1 lettre en majuscule, 1 lettre en minuscule et
+              1 caractère spécial.
             </p>
           </div>
           <div className="w-full flex flex-col items-center pt-4 pb-4">
-            <h3>Mon équipe</h3>
-            <hr className="h-[6px] w-2/3 rounded-full bg-bleu" />
+            <h3 className="mb-2">Mon équipe</h3>
+            <hr className="h-[6px] w-2/3 rounded-full bg-blue-enedis" />
           </div>
-          <p className="text-left w-3/4  mb-4">
+          <p className="text-left w-5/6  mb-4">
             Je travaille dans l&apos;équipe :
           </p>
-          <p className="flex items-center  w-3/4 border border-bleu rounded-full h-[32px] cursor-not-allowed ">
+          <p className="flex items-center  w-5/6 border border-blue-enedis rounded-full h-[32px] cursor-not-allowed ">
             <Image
               src="/assets/ENEDIS_PICTO_029_SerrageMains_BLEU_RVB_EXE 1.png"
               width={25}
@@ -151,7 +151,7 @@ function MyAccount() {
       </div>
       <button
         type="button"
-        className=" mt-8 text-center px-2 pt-1 w-3/4 rounded-full h-[35px] bg-vert text-white"
+        className="mt-4 text-center px-2 w-5/6 rounded-full h-[35px] bg-green-enedis text-white-enedis "
       >
         J&apos;enregistre les modifications
       </button>
