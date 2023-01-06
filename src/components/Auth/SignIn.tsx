af68b991369da65eda5f02087f64c38f5b25d335
e-genie <eugenie.froustey@gmail.com>
2023-01-06 15:57:55 +0100

    added tailwind config

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -19,16 +19,16 @@ function SignIn() {
   };
 
   return (
-    <div className="bg-[#1423DC] rounded-3xl text-white w-4/5 flex flex-col items-center space-y-8 py-6 sm:w-3/5 xl:w-2/5 xl:pb-10">
+    <div className="bg-blue-enedis rounded-connection-bloc text-white-enedis w-4/5 flex flex-col items-center space-y-8 py-6 sm:w-3/5 xl:w-2/5 xl:pb-10">
       <img src={logo.src} alt="logo" className="w-full" />
-      <div className="w-4/5 text-center text-sm">
+      <div className="w-4/5 text-center text-sm font-publicSans">
         Bienvenue sur la plateforme collaborative dédiée aux plans de
         communication d&apos;Enedis.
       </div>
-      <div className="text-xl font-bold">Je me connecte</div>
+      <div className="text-xl font-bold font-publicSans">Je me connecte</div>
       <div>
         <form>
-          <div className="flex flex-col space-y-3 text-black text-sm items-center justify-center">
+          <div className="flex flex-col space-y-3 text-black text-sm font-publicSans items-center justify-center">
             <div className="flex items-center space-x-3">
               <Image
                 src="/assets/enedis_picto_femme_blanc.png"
@@ -39,7 +39,7 @@ function SignIn() {
               <input
                 {...register("email", { required: true })}
                 placeholder="Mon adresse email"
-                className="rounded-3xl py-[6px] px-8 sm:px-10 lg:px-24"
+                className="rounded-connection-bloc py-[6px] px-8 sm:px-10 lg:px-24"
               />
             </div>
             <div className="flex items-center space-x-3">
@@ -52,22 +52,22 @@ function SignIn() {
               <input
                 {...register("password", { required: true })}
                 placeholder="Mon mot de passe"
-                className="rounded-3xl py-[6px] px-8 sm:px-10 lg:px-24"
+                className="rounded-connection-bloc py-[6px] px-8 sm:px-10 lg:px-24"
                 type="password"
               />
             </div>
           </div>
-          <div className="pt-10 pb-4 flex space-x-3 lg:justify-center lg:py-10">
+          <div className="pt-10 pb-4 flex font-bold space-x-3 justify-center lg:py-10">
             <button
               type="button"
-              className="border text-[10px] font-semibold rounded-full px-5"
+              className="border text-desk-xxs(mention) rounded-full px-5 py-2"
             >
               Mot de passe oublié ?
             </button>
             <button
               onClick={handleSubmit(onSubmit)}
               type="button"
-              className="bg-white text-[#1423DC] text-lg font-semibold rounded-full px-5"
+              className="bg-white-enedis text-blue-enedis text-desk-lg(CTA+input) rounded-full px-5"
             >
               Connexion
             </button>
