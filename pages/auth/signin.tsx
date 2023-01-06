291c16af6a12bb0bf9364a87238ff8bacae7450c
e-genie <eugenie.froustey@gmail.com>
2023-01-06 10:31:58 +0100

    responsive mobile / desktop OK

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -9,6 +9,7 @@ function signIn() {
         src="/assets/photo_connexion_mobile.png"
         width={1000}
         height={10}
+        quality={100}
         alt="an electrican man working in the background"
         className="sm:hidden"
       />
@@ -16,25 +17,28 @@ function signIn() {
         src="/assets/photo_connexion_desktop.jpg"
         width={10000}
         height={1}
+        quality={100}
         alt="an electrican man working in the background"
         className="hidden sm:flex md:absolute"
       />
-      <div className="absolute flex justify-center items-center top-28 md:justify-start md:left-36">
+      <div className="absolute z-50 flex justify-center items-center top-28 md:justify-start md:left-36">
         <SignIn />
       </div>
       <Image
         src="/assets/threads.png"
         width={1000}
         height={100}
+        quality={100}
         alt="decoration threads"
-        className="pt-32 lg:hidden"
+        className="pt-32 md:hidden"
       />
       <Image
         src="/assets/threads_desktop.png"
         width={10000}
         height={100}
+        quality={100}
         alt="decoration threads"
-        className="hidden md:flex md:relative md:top-60"
+        className="hidden md:flex z-0 relative md:top-60"
       />
     </div>
   );
