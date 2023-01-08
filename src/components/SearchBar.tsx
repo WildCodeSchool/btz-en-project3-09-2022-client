f36f51445b66c9de259c7a9c0a45ecf898dadd1e
LesCrow <john.baruteau@gmail.com>
2023-01-08 22:05:37 +0100

    users list modal in progress

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 import Image from "next/image";
+import useModal from "./modal/useModal";
+
+import Modal from "./modal/Modal";
 
 type TUser = {
   id: string;
@@ -18,8 +21,11 @@ type TUser = {
   workLocation: string;
 };
 
-function SearchBar({ width }: any) {
+function SearchBar() {
   const [selectedUser, setSelectedUser] = useState("");
+
+  const { isShowing, toggle } = useModal();
+
   // Fetch all users
   const getAllUsers = async () => {
     try {
@@ -39,36 +45,44 @@ function SearchBar({ width }: any) {
   if (error) {
     <p>Something bad happen</p>;
   }
+
+  const width = 760;
+
+  //    mobile :  "flex bg-green-enedis text-white-enedis rounded-select-mobile z-40 flex-col absolute top-20 right-16 w-1/3 md:w-[252px] md:ml-0 ml-[-30px] h-60 overflow-scroll "
+  // desk : "flex bg-green-enedis text-white-enedis rounded-select-mobile z-40 flex-col absolute top-16 right-20 w-5/6  ml-[-30px] h-60 overflow-scroll "
+
   return (
     <div className="flex items-center w-full">
+      <Modal isShowing={isShowing} hide={toggle}>
+        {users.length > 0 &&
+          users
+            .filter(
+              (user: TUser) =>
+                user.lastname.toLowerCase().includes(selectedUser) ||
+                user.firstname.toLowerCase().includes(selectedUser)
+            )
+            .map((user: TUser) => (
+              <span>
+                {user.firstname} {user.lastname}
+              </span>
+            ))}
+      </Modal>
       {width > 380 ? (
-        <>
-          {" "}
-          <input
-            type="select"
-            className="absolute right-[75px]   h-[45px] rounded-full text-center w-1/3 placeholder "
-            placeholder="Rechercher..."
-            onChange={(e) => setSelectedUser(e.target.value)}
-          />
-          <div className="flex bg-green-enedis text-white-enedis rounded-select-mobile z-40 flex-col absolute top-20 right-16 w-1/3 md:w-[252px] md:ml-0 ml-[-30px] h-60 overflow-scroll ">
-            {users.length > 0 &&
-              users
-                .filter((user: TUser) =>
-                  user.lastname.toLowerCase().includes(selectedUser)
-                )
-                .map((user: TUser) => (
-                  <span>
-                    {user.firstname} {user.lastname}
-                  </span>
-                ))}
-          </div>{" "}
-        </>
+        <input
+          type="select"
+          className="absolute right-[75px]   h-[45px] rounded-full text-center w-1/3 placeholder "
+          placeholder="Rechercher..."
+          onChange={(e) => setSelectedUser(e.target.value)}
+          onClick={toggle}
+        />
       ) : (
         <div className="relative  min-w-[50%] flex justify-between items-center  ">
           <input
             className="w-[86%] h-[40px] rounded-full text-center "
             placeholder="Rechercher sur Enedis Share..."
+            onChange={(e) => setSelectedUser(e.target.value)}
           />
+
           <Image
             src="/assets/ENEDIS_PICTO_003_Search_BLANC_EXE.png"
             width={1000}
