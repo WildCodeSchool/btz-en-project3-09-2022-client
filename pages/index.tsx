28930d741856ae6a06497b9d5cd40758c97b688e
LesCrow <john.baruteau@gmail.com>
2023-01-17 10:09:59 +0100

    rebase done

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Navbar from "../src/components/Navbar";
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <div className="flex flex-col justify-end h-screen">
         <Footer />
       </div>
