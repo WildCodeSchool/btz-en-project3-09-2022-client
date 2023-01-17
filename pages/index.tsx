c0cd4dd95dacf706af6a70a1b51f032427ef69c0
LesCrow <john.baruteau@gmail.com>
2023-01-17 10:19:37 +0100

    navbar on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import Footer from "../src/components/footer";
-
-import MyAccountMobile from "../src/components/MyAccountMobile";
-
 import Navbar from "../src/components/Navbar";
 
 export default function Home() {
