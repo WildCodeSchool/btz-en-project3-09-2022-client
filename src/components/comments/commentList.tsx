e3579160beb3ae818b315e33fc7b47b63f5116f8
e-genie <eugenie.froustey@gmail.com>
2023-01-25 15:11:08 +0100

    post comment OK, display comment after posting and input clean

--- src/components/comments/commentList.tsx
+++ src/components/comments/commentList.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
+import { TComment } from "../../types/main";
 import { commentFetcher } from "../../utils/fetcher";
 import CommentItem from "./commentItem";
 import CreateComment from "./createComment";
@@ -9,16 +10,25 @@ interface IProps {
 }
 
 function CommentList({ postId }: IProps) {
+  const [comments, setComments] = useState<TComment[]>([]);
+
   const { data, isLoading } = useQuery(["comments", `post-${postId}`], () =>
     commentFetcher.getAllByPostId(postId)
   );
+
+  const handleSubmit = (newComment: TComment) => {
+    setComments([...comments, newComment]);
+  };
 
   return (
     <div>
       {isLoading ? "Loading..." : null}
       {!data ? "No comment" : null}
-      {data && data.map((comment) => <CommentItem comment={comment} />)}
-      <CreateComment />
+      {data &&
+        data.map((comment) => (
+          <CommentItem key={comment.id} comment={comment} />
+        ))}
+      <CreateComment onSubmit={handleSubmit} />
     </div>
   );
 }
