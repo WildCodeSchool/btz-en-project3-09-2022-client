71898a9d93ecc7c166a05186800c4aaf08f32626
LesCrow <john.baruteau@gmail.com>
2023-01-25 17:11:34 +0100

    WIP3

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,21 +3,20 @@ import Link from "next/link";
 
 export default function FourOhFour() {
   return (
-    <>
+    <div>
       <Image
         src="/assets/confusedtravolta.jpg"
         fill
         quality={100}
         alt="not found"
-        className="relative opacity-"
+        className="relative  brightness-50"
       />
-      <div className="absolute">
-        <h1 className="font-enedis text-desk-4xl(welcome)">404</h1>
-        <p>There&apos;s nothing here.</p>
+      <div className="absolute  text-white-enedis top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-enedis text-[200px]">404</h1>
         <Link href="/">
           <p>Go back home</p>
         </Link>
       </div>
-    </>
+    </div>
   );
 }
