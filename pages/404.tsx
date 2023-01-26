cad8382616b3589ce5a22a661b595d25dea5d8f6
LesCrow <john.baruteau@gmail.com>
2023-01-26 13:49:58 +0100

    404 done

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,14 +5,15 @@ export default function FourOhFour() {
   return (
     <div>
       <Image
-        src="/assets/confusedtravolta.jpg"
+        src="/assets/74e92a6f41ebcac61b0ec795c5842716.gif"
         fill
-        quality={100}
         alt="not found"
-        className="relative  brightness-50"
+        className=" brightness-50 object-cover"
       />
       <div className="absolute  text-white-enedis top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className="font-enedis text-[200px]">404</h1>
+        <h1 className="font-enedis text-[150px]">404</h1>
+      </div>
+      <div className="absolute top-2/3 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white-enedis text-mob-3xl(welcome+name)">
         <Link href="/">
           <p>Go back home</p>
         </Link>
