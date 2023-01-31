08d353e92d3c414f8abcecc748b79af6e9cc4d3e
LesCrow <john.baruteau@gmail.com>
2023-01-31 15:10:57 +0100

    WIP

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../../src/components/layout/Layout";
 import Loader from "../../src/components/structureShared/Loader";
 import PostContent from "../../src/components/profil/PostContent";
