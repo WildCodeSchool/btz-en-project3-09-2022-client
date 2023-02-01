254bb57a6dec3aff1ed0829934e02c14877172ff
Tomahawkiwi <tlegendarme@gmail.com>
2023-02-01 13:30:56 +0100

    Integrated carousel in Space page, added a function to display or not carousel if no category except general. Added function for routing to category Page when click on carousel item.

--- src/components/spaces/structure/carouselCategory/ImageCategory.tsx
+++ src/components/spaces/structure/carouselCategory/ImageCategory.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { TCategory } from "../../../../types/main";
 
@@ -6,18 +8,27 @@ type Props = {
 };
 
 function ImageCategory({ category }: Props) {
+  const router = useRouter();
+  const { spaceId } = router.query;
+
   return (
-    <div className="relative z-0 w-[calc(100%-8px)] h-[48px] md:h-[48px] lg:px-0 mb-1">
-      <img
-        src={category.imageUrl}
-        alt={category.id}
-        className="rounded-app-bloc object-cover absolute z-10 h-full w-full "
-      />
-      <p className="absolute centered-absolute text-white-enedis text-desk-xs(date) md:text-desk-md(titlePubli+multiuse) z-40 font-enedis font-bold ">
-        {category.name}
-      </p>
-      <div className=" bg-blue-enedis mix-blend-hard-light opacity-[85%] relative z-30 rounded-app-bloc w-full h-full" />
-    </div>
+    <Link
+      key={category.id}
+      href={`${spaceId}/category/${category.id}`}
+      className="h-full w-full"
+    >
+      <div className="relative z-0 w-[calc(100%-8px)] h-[48px] md:h-[48px] lg:px-0 mb-1">
+        <img
+          src={category.imageUrl}
+          alt={category.id}
+          className="rounded-app-bloc object-cover absolute z-10 h-full w-full "
+        />
+        <p className="absolute centered-absolute text-white-enedis text-desk-xs(date) md:text-desk-md(titlePubli+multiuse) z-40 font-enedis font-bold ">
+          {category.name}
+        </p>
+        <div className=" bg-blue-enedis mix-blend-hard-light opacity-[85%] relative z-30 rounded-app-bloc w-full h-full" />
+      </div>
+    </Link>
   );
 }
 
